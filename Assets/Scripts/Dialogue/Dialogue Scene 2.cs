f8bc70fba960f8594ae2957429eea40801eed534
XtrEEmWasTaken <99416809+XtrEEmWasTaken@users.noreply.github.com>
2024-10-25 16:59:37 -0400

    donedene

--- Assets/Scripts/Dialogue/Dialogue Scene 2.cs
+++ Assets/Scripts/Dialogue/Dialogue Scene 2.cs
@@ -8,7 +8,6 @@ public class DialogueScene2 : MonoBehaviour
         public TextMeshProUGUI promptText;
 
     public string Dia2 = "Walking through the door you find your self in the same revine. You notice keen differences, the Door and trees are moved. hoping that you don't have to go through more doors you march on.";
-    public string Death = "Upon entering the door, it shuts behind you and locks. Looking further into the room, you witness yourself murder another version of yourself. He slowly turns around and looks at you while smiling. Then your vision slowly fades.";
 
 
     void Start()
