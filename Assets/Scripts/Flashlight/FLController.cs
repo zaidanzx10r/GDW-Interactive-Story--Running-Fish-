e75f0af50426e59e2b6b85751b14d16adc978e7d
XtrEEmWasTaken <99416809+XtrEEmWasTaken@users.noreply.github.com>
2025-02-09 16:14:51 -0500

    Flashlight Battery life
    Added a drain to the battery now take a bit of time for the flashlight to lose battery and turns off completely.
    
    Flashlight button is F
    Can be spammed
    Flashlight starts OFF

---
+++ Assets/Scripts/Flashlight/FLController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLController : MonoBehaviour
+{
+    public Light flashlight;
+    public float batteryLife = 100f;
+    public float batteryDrainSpeed = 1f;
+    private bool isOn = false;
+
+    void Start()
+    {
+        flashlight.enabled = isOn;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && batteryLife > 0)
+        {
+            isOn = !isOn;
+            flashlight.enabled = isOn;
+        }
+        
+        if(isOn)
+        {
+            batteryLife -= batteryDrainSpeed * Time.deltaTime;
+            if(batteryLife <= 0)
+            {
+                batteryLife = 0;
+                flashlight.enabled=false;
+                isOn = false;
+            }
+        }
+    }
+    public void RechargeBattery(float amount)
+    {
+        batteryLife += amount;
+        if(batteryLife > 100f)
+        {
+            batteryLife = 100f;
+        }
+    }
+}
