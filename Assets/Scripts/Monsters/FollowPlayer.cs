1172220798ef7da01482b5eb7e8e0172ec93744f
zaidanzx10r <zaidanvg.zx10r@gmail.com>
2025-03-10 11:49:10 -0400

    Added ai movement to deer
    Deer follows player and kills them using a collider

--- Assets/Scripts/Monsters/FollowPlayer.cs
+++ Assets/Scripts/Monsters/FollowPlayer.cs
@@ -2,14 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
 
+    public Behaviour deathScreen;
+
+    [SerializeField]
+    private float delay = 1f;
+
+    void Start()
+    {
+        deathScreen.enabled = false;
+    }
+
     void Update()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = player.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Hit Player");
+            StartCoroutine(ActionDelay(collision));
+        }
+    }
+
+    void ReloadMenuScene ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private IEnumerator ActionDelay(Collision player)
+    {
+        deathScreen.enabled = true;
+
+        yield return new WaitForSeconds(delay);
+
+        ReloadMenuScene();
     }
 }
