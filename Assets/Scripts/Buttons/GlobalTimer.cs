826f103b58bc786ad976a1c21d48656446297419
zaidanzx10r <zaidanvg.zx10r@gmail.com>
2025-03-17 00:20:02 -0400

    Formatted Timer

--- Assets/Scripts/Buttons/GlobalTimer.cs
+++ Assets/Scripts/Buttons/GlobalTimer.cs
@@ -19,11 +19,19 @@ public class GlobalTimer : MonoBehaviour
 
     public void Timer()
     {
-        scoreValue.text = ((int)currentTime).ToString();
-        finalScoreValue.text = ((int)currentTime).ToString();
-        currentTime -= removeTime * Time.deltaTime;
+        if (currentTime > 0)
+        {
+            currentTime -= removeTime * Time.deltaTime;
 
-        if (currentTime <= 0)
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+
+            string formattedTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            scoreValue.text = formattedTime;
+            finalScoreValue.text = formattedTime;
+        }
+        else
         {
             // make the player die
         }
